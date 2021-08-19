fce8e846e16b60e0b242afab622e245cb07a15d4
Alesandro Silva <silva_alesandro@outlook.com>
2021-08-18 21:53:19 -0400

    cria link para ocultar ou mostrar conteudo principal

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,22 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('[data-js=app]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector('[data-js="link"]')
+
+const app = document.querySelector('[class="app"]')
+
+link.innerText = 'Ocultar'
+
+link.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (app.style.display === 'none') {
+    app.style.display = 'block'
+    link.innerText = 'Ocultar'
+  } else {
+    app.style.display = 'none'
+    link.innerText = 'Mostrar'
+  }
+})
